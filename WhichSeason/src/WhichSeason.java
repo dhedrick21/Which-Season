a0ffb04fe673093468ff578f5c614c4a059625c9
dhedrick21 <43002631+dhedrick21@users.noreply.github.com>
2018-11-14 08:38:51 -0500

    Add files via upload

---
+++ WhichSeason/src/WhichSeason.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Dylan Hedrick
+ * Which Season
+ * 
+ */
+import java.util.Scanner;
+
+public class WhichSeason 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter your birth month(1-12): ");
+
+	}
+
+}
